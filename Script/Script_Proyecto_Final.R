99b86203b829741f84bb3b74dde6ad9822484a5c
JenVal94 <jenifferc.valbuena@gmail.com>
2023-07-30 21:27:23 +0000

    Cree la estructura del script

--- Script/Script_Proyecto_Final.R
+++ Script/Script_Proyecto_Final.R
@@ -1,1 +1,13 @@
 #Script Proyecto Final 
+
+#Descargar paquetes----
+install.packages("tidyverse")
+install.packages("ggplot2")
+
+#Cargar paquetes----
+library(tidyverse)
+library(openxlsx)
+library(ggplot2)
+library(tibble)
+
+#Crear tibble empresas de base
