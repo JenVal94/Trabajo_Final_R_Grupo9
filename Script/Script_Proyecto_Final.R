247ee0c5985253189190b6ab018f314b5948a649
JenVal94 <jenifferc.valbuena@gmail.com>
2023-07-30 21:34:05 +0000

    Estructura para codigo

--- Script/Script_Proyecto_Final.R
+++ Script/Script_Proyecto_Final.R
@@ -1,4 +1,5 @@
 #Script Proyecto Final 
+#Grupo 9: Denisse Ichau, Tamia Sisa, Jeniffer Valbuena
 
 #Descargar paquetes----
 install.packages("tidyverse")
@@ -10,4 +11,18 @@ library(openxlsx)
 library(ggplot2)
 library(tibble)
 
-#Crear tibble empresas de base
+#Crear tibble empresas de balance_2014----
+
+#Manipular base y crear dataframe 
+
+
+#Visualización balances 2014
+str(balances_2014)
+
+#Tabla resumen----
+
+#Grafico 1----
+
+#Grafico 2----
+
+
