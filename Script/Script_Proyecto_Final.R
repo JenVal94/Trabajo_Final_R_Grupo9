55888bbc1fc594a5c153b5898ff1c5103779b03d
JenVal94 <jenifferc.valbuena@gmail.com>
2023-08-03 00:56:06 +0000

    ultimos cambios

--- Script/Script_Proyecto_Final.R
+++ Script/Script_Proyecto_Final.R
@@ -5,17 +5,20 @@
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("dyplr")
+install.packages("magrittr")
 
 #Cargar paquetes----
 library(tidyverse)
 library(openxlsx)
 library(ggplot2)
 library(tibble)
+library(readxl)
+library(magrittr)
 
 #Crear tibble empresas de balance_2014----
 
 balances_2014 <- read.xlsx("Data/balances_2014.xlsx",detectDates=TRUE)
-balance_2014 <- data.frame()
+balances_2014 <- data.frame(balances_2014)
 
 #Manipular base y crear dataframe 
 
